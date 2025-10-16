5ef01e7be786fcd174f6a07d4a4c1b4a3f0402a4
simonecontidev <barriotropical@gmail.com>
2025-10-17 00:40:52 +0200

    slug NavLink
    Signed-off-by: simonecontidev <barriotropical@gmail.com>

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,17 +1,27 @@
-import React from 'react'
-import { Link } from '@mui/material'
+'use client';
+
+import React from 'react';
+import NextLink from 'next/link';
+import { Link, Typography } from '@mui/material';
 
 interface Props {
-    planetName: string;
-    planetId: number;
+  planetName: string;
+  planetId: number;
 }
 
-const NavLink = ({planetName, planetId}) => {
+const NavLink: React.FC<Props> = ({ planetName, planetId }) => {
   return (
-    <Link href={planetId}>
-      {planetName}
+    <Link
+      component={NextLink}
+      href={`/planets/${planetId}`}
+      underline="none"
+      sx={{ mr: 2, color: 'white', '&:hover': { opacity: 0.7 } }}
+    >
+      <Typography variant="body1" sx={{ fontWeight: 500, letterSpacing: 1 }}>
+        {planetName.toUpperCase()}
+      </Typography>
     </Link>
-  )
-}
+  );
+};
 
-export default NavLink
+export default NavLink;
